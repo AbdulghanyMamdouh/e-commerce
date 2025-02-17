b42dac9a75243c4ac86a40e08cec8e80b0d12043
Abdulghany Mamdouh <abdulghanymamdouh@gmail.com>
2025-02-18 00:21:42 +0200

    add theme,home screen and scree utils package to app

---
+++ lib/core/theme/color_manager.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primary = Color(0xFF004182);
+  static const Color darkPrimary = Color(0xFF004182);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color blueGrey = Color(0x4D004182);
+  static const Color yellow = Color(0xFFFDD835);
+  static const Color navy = Color(0x9906004F);
+  static const Color textColor = Color(0xff06004F);
+}
